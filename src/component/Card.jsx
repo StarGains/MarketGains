c2e55dbe19c242b08c41b5279cf1cb8081535b20
darkcheftar <darkcheftar.connect@gmail.com>
2023-09-04 19:48:48 +0530

    services styled

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,6 +1,6 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import "./Card.css";
 
 export default function Card({ card }) {
@@ -10,13 +10,15 @@ export default function Card({ card }) {
     return (
         // data - aos = "zoom-in" data - aos - duration="1500" 
         <div className="card-wrapper">
-            <img src={card.src} alt="" />
-            <h3>{card.heading}</h3>
-            <p>
-                Traders and Investors in India use our stable and reliable platforms regularlyTradings have risk and also brings a lot of fortune.
-                It was like rollercoaster that depends on the market to go forward.
-            </p>
-            {card.footer && <h3>{card.footer}</h3>}
+            <div className="card">
+                <img src={card.src} alt="" />
+                <h3>{card.heading}</h3>
+                <p>
+                    Traders and Investors in India use our stable and reliable platforms regularlyTradings have risk and also brings a lot of fortune.
+                    It was like rollercoaster that depends on the market to go forward.
+                </p>
+                {card.footer && <h3>{card.footer}</h3>}
+            </div>
         </div>
     )
 }
