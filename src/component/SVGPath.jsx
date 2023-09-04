0020485b7b23ea523f8b1e6f2aa4fb0e1bc06328
darkcheftar <darkcheftar.connect@gmail.com>
2023-09-04 18:29:50 +0530

    Hero Page Styled

--- src/component/SVGPath.jsx
+++ src/component/SVGPath.jsx
@@ -10,7 +10,7 @@ export default function SVGPath({ path, viewBox }) {
         }
     }, [pathRef])
     return <>
-        <svg style={{ position: 'absolute' }} width="800" height="800" viewBox={viewBox? viewBox: "-150 -100 300 200"} fill="none" xmlns="http://www.w3.org/2000/svg">
+        {false && <svg width="100%" height="100vh" viewBox={viewBox? viewBox: "-150 -100 300 200"} fill="none" xmlns="http://www.w3.org/2000/svg">
             <path ref={pathRef} d={path.d} stroke="grey" />
             <path strokeDasharray={pathLength} strokeDashoffset={pathLength} d={path.d} stroke="orange">
                 <animate attributeName="stroke-dashoffset" values={`0;${pathLength*2}`} dur="2s" begin="0" repeatCount="indefinite" />
@@ -18,6 +18,6 @@ export default function SVGPath({ path, viewBox }) {
             <path strokeDasharray={pathLength} strokeDashoffset={pathLength} d={path.d} stroke="grey">
                 <animate attributeName="stroke-dashoffset" values={`0;${pathLength*2}`} dur="2s" begin="0.2" repeatCount="indefinite" />
             </path>
-        </svg>
+        </svg>}
         </>
 }
